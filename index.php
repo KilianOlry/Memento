1267718a07565634240a98e21355577f66daf94b
Kilian Olry <kilian.olry@gmail.com>
2023-11-20 10:54:27 +0100

    Template terminer

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;700&family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/817262485e.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
     <header class="container">
-
             <a href="/">Memento</a>
-
         <nav>
             <ul>
                 <li>
@@ -29,8 +28,11 @@
             <a href="/" class="add-post-it">add post-it</a>
         <div class="content">
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
@@ -38,8 +40,11 @@
             </ul>
         </article>
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
@@ -47,8 +52,11 @@
             </ul>
         </article>
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
@@ -56,8 +64,11 @@
             </ul>
         </article>
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
@@ -65,8 +76,11 @@
             </ul>
         </article>
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
@@ -74,8 +88,11 @@
             </ul>
         </article>
         <article>
-            <h2>titre du post-it</h2>
-            <ul>
+            <div class="top-post-it">
+                <h2>titre du post-it</h2>
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <ul class="todo">
                 <li>test</li>
                 <li>test</li>
                 <li>test</li>
