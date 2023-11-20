791411ca237c11f3dbf99f70ea38e78175a31c58
Kilian Olry <kilian.olry@gmail.com>
2023-11-20 10:07:51 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;700&family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header class="container">
+
+            <a href="/">Memento</a>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Connexion</a>
+                </li>
+                <li>
+                    <a href="">Inscription</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h1>memento</h1>
+        <button>
+            <a href="/">add post-it</a>
+        </button>
+        <article>
+            <h2>titre du post-it</h2>
+            <ul>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+                <li>test</li>
+            </ul>
+        </article>
+    </main>
+</body>
+</html>
